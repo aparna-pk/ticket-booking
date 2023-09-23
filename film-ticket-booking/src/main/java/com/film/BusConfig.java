c9482e1b6c4c138c2c7ca394b70f72fd6cc5ea4c
Dell <Dell@MY-ROBO>
2023-09-23 18:26:20 +0530

    film booking

---
+++ film-ticket-booking/src/main/java/com/film/BusConfig.java
@@ -0,0 +1,25 @@
+package com.film;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.film.filter.BusFilter;
+
+@Configuration
+public class BusConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public BusFilter getFilter() {
+		return new BusFilter();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(getFilter()).addPathPatterns("/*");
+	}
+
+}
